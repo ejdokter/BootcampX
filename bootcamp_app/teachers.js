518bc7644e366b0bc4a821d799a9a6998718c5f7
Eric Dokter <edokter11@gmail.com>
2022-04-15 15:43:37 -0600

    add prevention for sql injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -13,12 +13,12 @@ FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`, [`%${process.argv[2]}%`])
 .then(res => {
-  res.rows.forEach(teacher => {
-    console.log(`${teacher.cohort}: ${teacher.teacher}`)
+  res.rows.forEach(rows => {
+    console.log(`${rows.cohort}: ${rows.teacher}`)
   })
 })
 .catch(err => console.error('query error', err.stack))
